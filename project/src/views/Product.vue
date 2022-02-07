857f5397c8b6be0dae77963fa41b787bb5fe363a
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-02-07 20:30:34 +0100

    created function `getProduct`

--- project/src/views/Product.vue
+++ project/src/views/Product.vue
@@ -1,9 +1,24 @@
 <template>
-	<article>tjo</article>
+	<article>{{ product }}</article>
 </template>
 <script>
 	export default {
-		name: 'Product'
+		name: 'Product',
+		data() {
+			return {
+				product: null
+			};
+		},
+		methods: {
+			async getProduct() {
+				const response = await fetch('/products.json');
+				const results = await response.json();
+				this.product = results.filter((o) => o.id === this.$route.params.id);
+			}
+		},
+		async created() {
+			this.getProduct();
+		}
 	};
 </script>
 <style scoped></style>
