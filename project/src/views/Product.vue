ee2b53ab68ed6f21fc22e49063eae88e1d93600b
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-02-07 21:35:47 +0100

    basic html structure for the products view

--- project/src/views/Product.vue
+++ project/src/views/Product.vue
@@ -1,5 +1,16 @@
 <template>
-	<article>{{ product }}</article>
+	<article v-if="product">
+		<section>
+			<img :src="'assets/products/' + product.images[0]" />
+		</section>
+		<section>
+			<router-link to="/product/d8907f9d-af50-49fe-afcc-1cd394aba056">{{ product.category }}</router-link>
+			<h2>{{ product.title }}</h2>
+			<p>{{ product.price }} <span>SEK</span></p>
+			<p>{{ product.description }}</p>
+			<p>{{ product.stock }}</p>
+		</section>
+	</article>
 </template>
 <script>
 	export default {
@@ -13,7 +24,7 @@
 			async getProduct() {
 				const response = await fetch('/products.json');
 				const results = await response.json();
-				this.product = results.filter((o) => o.id === this.$route.params.id);
+				this.product = results.find((o) => o.id === this.$route.params.id);
 			}
 		},
 		async created() {
