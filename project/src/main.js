11dcb53b5e9f58dacc9a87208140edbea2634fdf
Anna Eriksson <annaeriksson0516@gmail.com>
2022-02-07 12:04:43 +0100

    Icons, PWA, service worker

--- project/src/main.js
+++ project/src/main.js
@@ -3,5 +3,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import { registerSW } from 'virtual:pwa-register';
 
 createApp(App).use(router).use(store).mount('#app');
+registerSW();
