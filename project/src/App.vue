eb53f434dfa35b1627e86f6bb57d24ed56664169
Anna Eriksson <annaeriksson0516@gmail.com>
2022-02-07 11:42:42 +0100

    Add logo image

--- project/src/App.vue
+++ project/src/App.vue
@@ -2,29 +2,21 @@
 	import CitiesList from './components/CitiesList.vue';
 	import CounterButton from './components/CounterButton.vue';
 	import HelloWorld from './components/HelloWorld.vue';
+	import NavBar from './components/NavBar.vue';
 
 	export default {
 		components: {
 			CitiesList,
 			CounterButton,
-			HelloWorld
+			HelloWorld,
+			NavBar
 		}
 	};
 </script>
 
 <template>
 	<nav>
-		<ul>
-			<li>
-				<RouterLink to="/">Hem</RouterLink>
-			</li>
-			<li>
-				<RouterLink to="/about">Om</RouterLink>
-			</li>
-			<li>
-				<RouterLink to="/contact">Kontakt</RouterLink>
-			</li>
-		</ul>
+		<NavBar />
 	</nav>
 	<HelloWorld msg="Hello World!" />
 	<CitiesList />
---
+++ project/src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<style lang="scss"></style>
+<template>
+	<div id="nav">
+		<nav class="navbar navbar-expand-lg navbar-light">
+			<div class="navbar-nav mr-auto">
+				<router-link to="/" class="nav-item nav-link">Hem</router-link>
+				<router-link to="/about" class="nav-item nav-link">Om</router-link>
+				<router-link to="/contact" class="nav-link">Kontakt</router-link>
+			</div>
+		</nav>
+	</div>
+</template>
