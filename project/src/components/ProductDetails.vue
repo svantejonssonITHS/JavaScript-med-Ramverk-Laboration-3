0bdb1d1e87783fa005a3ee8634e0ff0921164bdb
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-02-08 19:15:50 +0100

    Create ProductDetails.vue

---
+++ project/src/components/ProductDetails.vue
@@ -0,0 +1,54 @@
+<template>
+	<section>
+		<router-link to="/product/d8907f9d-af50-49fe-afcc-1cd394aba056" class="text-muted text-underline-hover"
+			>{{ category }}/</router-link
+		>
+		<h2 class="fw-bold">{{ title }}</h2>
+		<p>{{ price }} <span>SEK</span></p>
+		<p>{{ description }}</p>
+		<p>{{ stock }}</p>
+	</section>
+</template>
+
+<script>
+	export default {
+		name: 'ProductDetails',
+		props: {
+			title: {
+				type: String,
+				required: true,
+				default: 'N/A'
+			},
+			category: {
+				type: String,
+				required: true,
+				default: 'N/A'
+			},
+			description: {
+				type: String,
+				required: true,
+				default: 'N/A'
+			},
+			price: {
+				type: Number,
+				required: true,
+				default: -1
+			},
+			stock: {
+				type: Number,
+				required: true,
+				default: 0
+			}
+		}
+	};
+</script>
+
+<style scoped>
+	.text-underline-hover {
+		text-decoration: none;
+	}
+
+	.text-underline-hover:hover {
+		text-decoration: underline;
+	}
+</style>
