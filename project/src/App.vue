cd94541b2aded9dac12041a8db770ceddd6082ee
Anna Eriksson <annaeriksson0516@gmail.com>
2022-02-08 09:54:42 +0100

    Fixed change in import

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,23 +1,10 @@
 <script>
-<<<<<<< HEAD
-    import FooterSection from './components/FooterSection.vue';
-	import CitiesList from './components/CitiesList.vue';
-	import CounterButton from './components/CounterButton.vue';
-
-=======
->>>>>>> ee2b53ab68ed6f21fc22e49063eae88e1d93600b
 	import NavBar from './components/NavBar.vue';
 	import HeaderImage from './components/HeaderImage.vue';
 	import FooterSection from './components/FooterSection.vue';
 
 	export default {
 		components: {
-<<<<<<< HEAD
-			CitiesList,
-			CounterButton,
-            FooterSection,
-=======
->>>>>>> ee2b53ab68ed6f21fc22e49063eae88e1d93600b
 			NavBar,
 			HeaderImage,
 			FooterSection
