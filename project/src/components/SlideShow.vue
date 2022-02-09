1553312276ada10fa524d770053b2527226885bf
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-02-09 21:49:39 +0100

    styled the slideshow

--- project/src/components/SlideShow.vue
+++ project/src/components/SlideShow.vue
@@ -1,6 +1,11 @@
 <template>
-	<section>
+	<section class="d-flex flex-column">
 		<img :src="imgShown" alt="Product image" class="w-100" />
+		<nav v-if="images.length > 1" class="mw-100 d-flex horizontal-scrollable p-2">
+			<button v-for="(image, index) in this.images" :key="index" class="btn col-2 flex-grow-1">
+				<img :src="`assets/products/${image}`" class="w-100 rounded" />
+			</button>
+		</nav>
 	</section>
 </template>
 
@@ -29,4 +34,12 @@
 	};
 </script>
 
-<style scoped></style>
+<style scoped>
+	nav {
+		overflow-x: scroll;
+	}
+
+	.activeImg {
+		filter: brightness(75%);
+	}
+</style>
