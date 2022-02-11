862ad1224bd652a4c68ab0e5f1cecfeeac90ae2c
Anna Eriksson <annaeriksson0516@gmail.com>
2022-02-11 15:25:38 +0100

    Working on carousel component, added images

---
+++ project/src/components/Carousel.vue
@@ -0,0 +1,78 @@
+<template>
+	<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img
+					src="../../assets/products/032cbc00-e00c-46b5-8a37-88ac7bd66d96/0.jpg"
+					class="d-block w-100"
+					alt="watch"
+				/>
+			</div>
+			<div class="carousel-item">
+				<img
+					src="../../assets/products/0cd2edc6-cbfa-440b-9c09-1c569042c69b/0.jpg"
+					class="d-block w-100"
+					alt="sun glasses"
+				/>
+			</div>
+			<div class="carousel-item">
+				<img
+					src="../../assets/products/2029ce36-b763-4856-8a6b-dc79d2f0a8c6/0.jpg"
+					class="d-block w-100"
+					alt="wallet brown"
+				/>
+			</div>
+			<div class="carousel-item">
+				<img
+					src="../../assets/products/23a1cdad-9c66-49ba-aacf-01e43c9ce869/0.jpg"
+					class="d-block w-100"
+					alt="wallet black"
+				/>
+			</div>
+			<div class="carousel-item">
+				<img
+					src="../../assets/products/4ab4db67-9614-4908-b563-63cf42585749/0.jpg"
+					class="d-block w-100"
+					alt="watch black"
+				/>
+			</div>
+			<div class="carousel-item">
+				<img
+					src="../../assets/products/59555930-be69-466f-91dc-2f564c26cc4b/0.jpg"
+					class="d-block w-100"
+					alt="earrings"
+				/>
+			</div>
+			<div class="carousel-item">
+				<img
+					src="../../assets/products/681ba08e-10cc-4ac4-94c2-53b8f6f3d7f0/0.jpg"
+					class="d-block w-100"
+					alt="watch on arm"
+				/>
+			</div>
+		</div>
+		<button
+			class="carousel-control-prev"
+			type="button"
+			data-bs-target="#carouselExampleControls"
+			data-bs-slide="prev"
+		>
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Previous</span>
+		</button>
+		<button
+			class="carousel-control-next"
+			type="button"
+			data-bs-target="#carouselExampleControls"
+			data-bs-slide="next"
+		>
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Next</span>
+		</button>
+	</div>
+</template>
+<style lang="scss">
+	img {
+		max-width: 50%;
+	}
+</style>
--- project/src/views/Home.vue
+++ project/src/views/Home.vue
@@ -1,3 +1,12 @@
 <template>
 	<h1>Hem</h1>
+	<Carousel />
 </template>
+<script>
+	import Carousel from '../components/Carousel.vue';
+	export default {
+		components: {
+			Carousel
+		}
+	};
+</script>
