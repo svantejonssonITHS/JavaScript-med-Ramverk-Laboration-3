a2a2c7f47db4451ff32506e0267bc52da3b67214
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-02-12 15:35:08 +0100

    basic view layout

--- project/src/views/Cart.vue
+++ project/src/views/Cart.vue
@@ -1,4 +1,12 @@
-<template>Cart</template>
+<template>
+	<article>
+		<h2>Din kundvagn</h2>
+		<ul>
+			<li>Produkt</li>
+		</ul>
+		<button>Till kassan</button>
+	</article>
+</template>
 <script>
 	export default {
 		name: 'Cart'
