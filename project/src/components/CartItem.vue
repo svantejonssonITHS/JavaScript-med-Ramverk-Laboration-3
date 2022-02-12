e1945bb91df0122bd637ba6be202c22762149623
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-02-12 16:04:59 +0100

    added props to the `CartItem` component

--- project/src/components/CartItem.vue
+++ project/src/components/CartItem.vue
@@ -1,10 +1,39 @@
 <template>
-	<section></section>
+	<router-link to="/">
+		<section>
+			<img src="" />
+			<h3>Namn</h3>
+			<section>amount</section>
+			<p>pris</p>
+		</section>
+	</router-link>
 </template>
 
 <script>
 	export default {
-		name: 'CartItem'
+		name: 'CartItem',
+		props: {
+			id: {
+				type: String,
+				required: true
+			},
+			name: {
+				type: String,
+				required: true
+			},
+			imgUrl: {
+				type: String,
+				required: true
+			},
+			amount: {
+				type: Number,
+				required: true
+			},
+			price: {
+				type: Number,
+				required: true
+			}
+		}
 	};
 </script>
 
