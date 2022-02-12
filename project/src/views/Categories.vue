7fea098313be644d0c6e66b48ea84f82b216f281
Jazz-21 <tiwaryjoya@gmail.com>
2022-02-12 17:41:48 +0100

    Filter functioning on Category

--- project/src/views/Categories.vue
+++ project/src/views/Categories.vue
@@ -1,3 +1,14 @@
+<script>
+	import FilterSearch from './../components/FilterSearch.vue';
+
+	export default {
+		components: {
+			FilterSearch
+		}
+	};
+</script>
+
 <template>
+	<FilterSearch />
 	<h1>Kategorier</h1>
 </template>
