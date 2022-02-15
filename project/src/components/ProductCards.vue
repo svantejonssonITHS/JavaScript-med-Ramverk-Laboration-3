01aa08e50457cb981c6da7fba80a9b945f1f6196
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-02-15 16:10:14 +0100

    fixed image urls

--- project/src/components/ProductCards.vue
+++ project/src/components/ProductCards.vue
@@ -18,7 +18,7 @@
 	<div class="row">
 		<div class="col-md-6">
 			<div class="card" v-for="product in products" :key="product.id" style="width: 18rem">
-				<img :src="product.images[0]" class="card-img-top" alt="..." />
+				<img :src="'assets/products/' + product.images[0]" class="card-img-top" alt="..." />
 				<div class="card-body">
 					<h5 class="card-title">{{ product.title }}</h5>
 					<p class="card-text">
