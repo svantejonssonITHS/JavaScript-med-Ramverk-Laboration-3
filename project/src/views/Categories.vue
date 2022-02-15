f69bc0172ff59897c9bb0fa9407cfb5df636d9f1
Frida Salahshorian <frida.salahshorian@gmail.com>
2022-02-15 12:49:50 +0100

    eddit categories.vue

--- project/src/views/Categories.vue
+++ project/src/views/Categories.vue
@@ -1,3 +1,18 @@
 <template>
 	<h1>Kategorier</h1>
 </template>
+
+<script>
+	fetch('https://request.in/api/categories', {
+		method: 'POST',
+		headers: { 'Content-Type': 'product/json' },
+		body: JSON.stringify({ name: 'Armband' })
+	})
+		.then((res) => {
+			return res.json();
+		})
+		.then((data) => console.log(data))
+		.catch((error) => console.log('ERROR'));
+</script>
+
+<style></style>
