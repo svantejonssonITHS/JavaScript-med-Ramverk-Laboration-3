75f6aff50ed29306d5778e022ea60f61518c7c2d
Tartaros1 <sabahchamoun@hotmail.com>
2022-02-15 11:11:03 +0100

    Fetch image

---
+++ project/src/components/ImageFetch.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="info">
+		<ImageFetch class="infoPic" />
+		<p></p>
+	</div>
+</template>
+
+<script>
+	export default {
+		created() {
+			let promise = fetch('/products.json');
+
+			promise.then((response) => {
+				this.products = response.json();
+			});
+		},
+
+		data() {
+			return {
+				products: ''
+			};
+		},
+		props: {
+			images: { type: String, required: true }
+		}
+	};
+</script>
