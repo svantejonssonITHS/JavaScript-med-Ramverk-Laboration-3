994bea3228c06be1400a6bf5d4a39ae8a0e69cc7
Anna Eriksson <annaeriksson0516@gmail.com>
2022-02-16 17:50:51 +0100

    Fixed ImageFetch to render two images from json

--- project/src/components/ImageFetch.vue
+++ project/src/components/ImageFetch.vue
@@ -1,15 +1,17 @@
 <template>
-	<div class="descript">
-		<img :src="images" class="infoPic" />
-		<p>
-			Accesories by 5 is the world’s largest jewellery brand. The company designs, manufactures and markets hand-finished
-			jewellery made from high-quality materials at affordable prices. A5 jewellery is sold in more than 100 countries
-			through 6,800 points of sale, including more than 2,600 concept stores.
-		</p>
-	</div>
-	<div class="descripti">
-		<img :src="images" class="infoPic" />
-		<p>Check out our wonderful necklace beads</p>
+	<div class="descript" v-for="(product, index) in products" :key="product.id">
+		<div v-if="index === 0">
+			<img :src="'assets/products/' + product.images[0]" class="infoPic" />
+			<p>
+				Accesories by 5 is the world’s largest jewellery brand. The company designs, manufactures and markets
+				hand-finished jewellery made from high-quality materials at affordable prices. A5 jewellery is sold in more than
+				100 countries through 6,800 points of sale, including more than 2,600 concept stores.
+			</p>
+		</div>
+		<div v-if="index === 1">
+			<img :src="'assets/products/' + product.images[0]" class="infoPic" />
+			<p>Exempeltext 2</p>
+		</div>
 	</div>
 </template>
 <style scoped>
@@ -19,23 +21,18 @@
 	}
 </style>
 <script>
+	import axios from 'axios';
+
 	export default {
 		created() {
-			let promise = fetch('/products.json');
-			promise
-				.then((response) => response.json())
-				.then(function (result) {
-					this.products = result;
-				});
+			axios.get('/products.json').then((response) => (this.products = response.data));
+			console.log(this.products);
 		},
 
 		data() {
 			return {
 				products: ''
 			};
-		},
-		props: {
-			images: { type: String, required: true }
 		}
 	};
 </script>
--- project/src/views/Home.vue
+++ project/src/views/Home.vue
@@ -5,7 +5,7 @@
 		<h2>About our company</h2>
 		<p>Our history</p>
 	</section>
-	<ImageFetch v-if="products" :images="`src/assets/${products.images}`" />
+
 	<ImageFetch />
 </template>
 
