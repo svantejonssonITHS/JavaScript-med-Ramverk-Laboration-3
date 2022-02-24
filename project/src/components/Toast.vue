71560606781a6d0bd7fa67577d66e99862433f6b
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-02-24 14:27:00 +0100

    added props to `Toast.vue`

--- project/src/components/Toast.vue
+++ project/src/components/Toast.vue
@@ -2,7 +2,50 @@
 
 <script>
 	export default {
-		name: 'Toast'
+		name: 'Toast',
+		props: {
+			title: {
+				type: String,
+				required: false,
+				default: 'Notice'
+			},
+			message: {
+				type: String,
+				required: true
+			},
+			type: {
+				type: String,
+				required: false,
+				default: 'info',
+				validator(value) {
+					// Available types to choose from
+					return ['info', 'success', 'warning', 'danger'].includes(value);
+				}
+			},
+			position: {
+				type: Array,
+				required: false,
+				// X-axis: end
+				// Y-axis: start
+				default: () => ['end', 'start'],
+				validator(value) {
+					// An array of two strings is expected
+					if (value.length != 2) return false;
+
+					// Available options
+					const options = ['start', 'middle', 'end'];
+
+					if (!options.includes(value[0])) return false;
+					if (!options.includes(value[1])) return false;
+					return true;
+				}
+			},
+			duration: {
+				type: Number,
+				required: false,
+				default: 2500 // 2500ms (2.5s)
+			}
+		}
 	};
 </script>
 
