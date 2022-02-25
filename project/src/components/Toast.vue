c7e9d258553e8b10f16e0eec3adb09d0744d2603
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-02-25 10:20:34 +0100

    styled Toast

--- project/src/components/Toast.vue
+++ project/src/components/Toast.vue
@@ -1,4 +1,16 @@
-<template>Toast</template>
+<template>
+	<article class="zindex-modal shadow m-4 position-fixed top-0 end-0">
+		<header class="info d-flex justify-content-between align-items-center p-2 rounded-top">
+			<h2 class="m-0 p-0 fs-5">{{ title }}</h2>
+			<button class="info btn rounded-circle d-flex align-items-center fs-5">
+				<i class="fa fa-times"></i>
+			</button>
+		</header>
+		<main class="bg-white p-2 rounded-bottom">
+			<p class="m-0 p-0 pe-5 fs-5">{{ message }}</p>
+		</main>
+	</article>
+</template>
 
 <script>
 	export default {
@@ -49,4 +61,29 @@
 	};
 </script>
 
-<style scoped></style>
+<style scoped>
+	.info {
+		background-color: #a7c7e7;
+		color: #212529;
+	}
+
+	.success {
+		background-color: #198754;
+		color: #f8f9fa;
+	}
+
+	.warning {
+		background-color: #ffc107;
+		color: #212529;
+	}
+
+	.danger {
+		background-color: #dc3545;
+		color: #f8f9fa;
+	}
+
+	button {
+		height: 100% !important;
+		aspect-ratio: 1/1;
+	}
+</style>
