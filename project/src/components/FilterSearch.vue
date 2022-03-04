e5251c03cb76b63d5741640108dbcf21df431f53
Jazz-21 <tiwaryjoya@gmail.com>
2022-03-04 23:20:32 +0100

    final code for filter which includes Sorting by text and price

--- project/src/components/FilterSearch.vue
+++ project/src/components/FilterSearch.vue
@@ -1,3 +1,29 @@
+<template>
+	<div class="overflow-scroll">
+		<div class="search col row-cols-1">
+			<input class="searchInput" type="text" v-model="search" placeholder="Search Your Products" />
+		</div>
+		<div class="buttons">
+			<input class="btn btn-dark" @click="onAsc" type="button" value="Price-Low-high" />
+			<input class="btn btn-dark" @click="onDsc" type="button" value=" Price-High-Low" />
+		</div>
+
+		<div class="wrapper row row-cols-1 row-cols-md-3 g-4">
+			<div class="card" v-for="post in filteredList" :key="post.id">
+				<img :src="'assets/products/' + post.images[0]" class="card-img-top" alt="..." />
+				<div class="card-body">
+					<h5 class="card-title">{{ post.title }}</h5>
+					<p class="card-text">
+						{{ post.description }}
+					</p>
+					<h5>{{ post.price }} SEK</h5>
+					<router-link :to="`/product/${post.id}`" class="btn btn-dark">Info</router-link>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
 <script>
 	export default {
 		created() {
@@ -17,28 +43,57 @@
 		data() {
 			return {
 				products: null,
-				search: ''
+				search: '',
+				sortDirection: 0
 			};
 		},
 		computed: {
 			filteredList() {
-				return this.products.filter((post) => {
-					return post.title.toLowerCase().includes(this.search.toLowerCase());
-				});
+				if (!this.products) return;
+				return this.products
+					.filter((post) => {
+						return post.title.toLowerCase().includes(this.search.toLowerCase());
+					})
+					.sort((a, b) => {
+						return (a.price - b.price) * this.sortDirection;
+					});
+			}
+		},
+		methods: {
+			onAsc() {
+				this.sortDirection = +1;
+			},
+			onDsc() {
+				this.sortDirection = -1;
 			}
 		}
 	};
 </script>
 
-<template>
-	<div class="search-wrapper">
-		<input type="text" v-model="search" placeholder="Search title.." />
-	</div>
-	<div class="wrapper">
-		<div class="card" v-for="post in filteredList" :key="post.id">
-			<a target="_blank">
-				{{ post.title }}
-			</a>
-		</div>
-	</div>
-</template>
+<style lang="scss" scoped>
+	.overflow-scroll {
+		max-height: 75vh;
+		overflow-x: hidden !important;
+	}
+	.card {
+		margin: 2em 2em;
+		width: 20em;
+		transition: all 0.3s ease-out;
+	}
+	.card:hover {
+		transform: translateY(-5px) scale(1.005) translateZ(0);
+		box-shadow: 0 24px 36px rgba(0, 0, 0, 0.11), 0 24px 46px rgba(255, 215, 97, 0.48);
+	}
+	.search {
+		margin: 3em;
+		align-content: center;
+	}
+	.searchInput {
+		padding: 0.5em;
+		text-align: center;
+	}
+	.buttons {
+		margin-bottom: 40px;
+		margin-left: 40px;
+	}
+</style>
