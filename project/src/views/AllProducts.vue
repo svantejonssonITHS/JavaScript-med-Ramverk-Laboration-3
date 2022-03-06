b6df33226b0af739de23a5a1f1187dc5a70a309d
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-03-06 18:41:32 +0100

    fixed Vue tag orders

--- project/src/views/AllProducts.vue
+++ project/src/views/AllProducts.vue
@@ -1,3 +1,7 @@
+<template>
+	<h1 class="display-3">Alla produkter</h1>
+	<ProductCards />
+</template>
 <script>
 	import ProductCards from '../components/ProductCards.vue';
 	export default {
@@ -6,7 +10,3 @@
 		}
 	};
 </script>
-<template>
-	<h1 class="display-3">Alla produkter</h1>
-	<ProductCards />
-</template>
